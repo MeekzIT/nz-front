776189cdff06d72e316fa250958d5a8b06ce78e1
gevorg <gevorg@branchdev.io>
2025-02-25 23:06:41 +0400

    add translations

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -1,6 +1,9 @@
 'use client';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { FC, PropsWithChildren, useState } from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '@/i18n';
 
 const Providers: FC<PropsWithChildren> = ({ children }) => {
   const [queryClient] = useState(
@@ -16,7 +19,11 @@ const Providers: FC<PropsWithChildren> = ({ children }) => {
       }),
   );
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default Providers;
